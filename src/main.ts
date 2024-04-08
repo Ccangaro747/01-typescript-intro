61154903cede03c63ba39124cc791eaa232d225e
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-08 13:50:25 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types.ts';
-import './topics/02-object-interface.ts';
+//import './topics/02-object-interface.ts';
+import './topics/03-functions.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hola Mundo
---
+++ src/topics/03-functions.ts
@@ -0,0 +1,25 @@
+function addNumbers(a: number, b: number): number{
+    return a + b;
+}
+
+const addNumbersArrow = (a: number, b: number):string => {
+    return `${a + b}`;
+}
+
+
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
+    return firstNumber * base;
+}
+
+
+const result: number = addNumbers(1, 2);
+const result2: string = addNumbersArrow(1, 2);
+const multiplyResult: number = multiply(5);
+
+
+console.log({result, result2, multiplyResult});
+
+
+addNumbers(1, 2);
+
+export{};
