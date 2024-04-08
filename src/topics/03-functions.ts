5338bd11864f795848e1b067191fd8f1fa02f66c
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-08 19:00:15 -0300

    Agregadas funciones de curación para el personaje Strider

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -12,12 +12,36 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
 }
 
 
-const result: number = addNumbers(1, 2);
+/*const result: number = addNumbers(1, 2);
 const result2: string = addNumbersArrow(1, 2);
 const multiplyResult: number = multiply(5);
+console.log({result, result2, multiplyResult});*/
 
 
-console.log({result, result2, multiplyResult});
+interface Character{
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
 
+
+const healCharacter = ( character: Character, amount: number) =>{
+
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 100,
+    showHp(){
+        console.log(`HP: ${this.hp}`);
+    }
+}
+
+
+healCharacter(strider, 20);
+healCharacter(strider, 10);
+
+strider.showHp();
 
 export{};
